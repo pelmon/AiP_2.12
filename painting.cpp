2d3ecc9f2ef961f0789acb2c8203b31650bd0ad1
pelmon <kacuraarsenij@gmail.com>
2025-12-02 09:36:50 +0300

    First commit

---
+++ painting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+namespace topit{
+    struct p_t{
+    int x, y;
+};
+    bool operator == (p_t a, p_t b)
+    bool operator != (p_t a, p_t b)
+}
+int main()
+{
+    p_t a{1, 1}, b{0, 1};
+    std::cout << (a==b) << "/n";
+}
+
+bool topit::operator == (p_t a, p_t b){
+    return a.x == b.x && a.y == b.y;
+}
+
+bool topit::operator != (p_t a, p_t b){
+    return !(a==b);
+}
