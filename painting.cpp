7274ae34465f742892a4a03efac9032b37af7452
pelmon <kacuraarsenij@gmail.com>
2025-12-02 11:22:06 +0300

    canvas() writen

--- painting.cpp
+++ painting.cpp
@@ -2,6 +2,8 @@
 namespace topit{
     struct p_t{ int x, y;};
     struct f_t{p_t aa, bb;};
+
+
     bool operator == (p_t a, p_t b);
     bool operator != (p_t a, p_t b);
     struct IDraw{
@@ -52,6 +54,14 @@ int main()
     delete shp[0];
     return err;
 }
+char * topit::canvas(f_t fr,  char fill){
+    size_t s rows(fr) * cols(fr);
+    char * c = new char[s];
+    for(size_t i = 0; i < s; ++i){
+        c[i] = fill;
+    }
+    return c;
+}
 topit::f_t topit::frame(const p_t* pts, size_t s){
     int minx = pts[0].x, miny = pts[0].y;
     int maxx = minx, maxy = miny;
@@ -77,6 +87,12 @@ topit::p_t topit::Dot::next(p_t prev) const {
         throw std::logic_error("bad prev");
     }
     return d;
+}
+size_t topit::rows(f_t ft){
+    return (fr.bb.y - fr.aa.y + 1);
+}
+size_t topit::cols(f_t fr){
+    return (fr.bb.x - fr.aa.x + 1);
 }
 bool topit::operator == (p_t a, p_t b){
     return a.x == b.x && a.y == b.y;
